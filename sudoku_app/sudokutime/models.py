fbd0eff2eb5d82421973c34e15caf4448543a7f6
carlschr <cjcarlson136@gmail.com>
2021-10-13 12:23:55 -0500

    Create admin user, urls, db

--- sudoku_app/sudokutime/models.py
+++ sudoku_app/sudokutime/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Sudoku(models.Model):
+    unsolved = models.CharField(max_length=81)
+    solved = models.CharField(max_length=81, primary_key=True, unique=True)
+    positive_ratings = models.IntegerField(default=0)
+    negative_ratings = models.IntegerField(default=0)
+    timestamp = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.solved
