6caf390af4fe271c0553a4614267131758c5497b
carlschr <cjcarlson136@gmail.com>
2021-10-20 12:48:52 -0500

    Added max_length  to char fields

--- sudoku_app/sudokutime/models.py
+++ sudoku_app/sudokutime/models.py
@@ -3,8 +3,8 @@ from django.core.validators import RegexValidator
 
 # Create your models here.
 class Sudoku(models.Model):
-    unsolved = models.CharField(validators=[RegexValidator(regex='^[1-9\.]{81}$', message='Length has to be 81', code='nomatch')])
-    solved = models.CharField(validators=[RegexValidator(regex='^[1-9]{81}$', message='Length has to be 81', code='nomatch')], primary_key=True, unique=True)
+    unsolved = models.CharField(max_length=81, validators=[RegexValidator(regex='^[1-9\.]{81}$', message='Length has to be 81', code='nomatch')])
+    solved = models.CharField(max_length=81, validators=[RegexValidator(regex='^[1-9]{81}$', message='Length has to be 81', code='nomatch')], primary_key=True, unique=True)
     positive_ratings = models.IntegerField(default=0)
     negative_ratings = models.IntegerField(default=0)
     timestamp = models.DateTimeField('date created')
