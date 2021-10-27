61e1c9145a0509a810c079f4d4bf5849c86c7951
carlschr <cjcarlson136@gmail.com>
2021-10-27 14:54:46 -0500

    Chnaged implementation of generate_sudoku

--- sudoku_app/sudokutime/views.py
+++ sudoku_app/sudokutime/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    from .utils.generate_sudoku import new_sudoku, new_sudoku_solved
+    from .utils.generate_sudoku import generate_sudoku
+    new_sudoku, new_sudoku_solved = generate_sudoku()
+    
     boxes = []
     for i in range(9):
         off = (i % 3) * 3 + (i - (i % 3)) * 9
@@ -11,5 +13,6 @@ def index(request):
         col = i % 3 + 1
         grid_area = f'grid-area: {row}/{col}/{row + 1}/{col + 1}'
         boxes.append({'box': new_box, 'grid_area': grid_area})
+
     args = {'new_sudoku': new_sudoku, 'new_sudoku_solved': new_sudoku_solved, 'boxes': boxes}
     return render(request, 'index.html', args)
