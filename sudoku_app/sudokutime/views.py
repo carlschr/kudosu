d98623881847079fec3ed72d5296a2825570e638
carlschr <cjcarlson136@gmail.com>
2021-10-27 14:46:52 -0500

    Moved grid generation to template

--- sudoku_app/sudokutime/views.py
+++ sudoku_app/sudokutime/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     from .utils.generate_sudoku import new_sudoku, new_sudoku_solved
-    args = {'new_sudoku': new_sudoku, 'new_sudoku_solved': new_sudoku_solved}
+    boxes = []
+    for i in range(9):
+        off = (i % 3) * 3 + (i - (i % 3)) * 9
+        new_box = new_sudoku[0+off:3+off] + new_sudoku[9+off:12+off] + new_sudoku[18+off:21+off]
+        row = int(i//3) + 1
+        col = i % 3 + 1
+        grid_area = f'grid-area: {row}/{col}/{row + 1}/{col + 1}'
+        boxes.append({'box': new_box, 'grid_area': grid_area})
+    args = {'new_sudoku': new_sudoku, 'new_sudoku_solved': new_sudoku_solved, 'boxes': boxes}
     return render(request, 'index.html', args)
